c455fc3f0e61f83beeed03aa1003b069310782b3
Teguh Firmansyah <teguhfirmansyah@Teguhs-MacBook-Pro.local>
2020-05-05 16:29:50 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -27,13 +27,9 @@ Route::group(['prefix' => 'admin'], function () {
 	Route::get('/mapel', 'MapelController@index')->name('mapel.index');
 	Route::post('/mapel', 'MapelController@importMapel')->name('mapel.import');
 
-<<<<<<< HEAD
-Route::get('/siswa', 'SiswaController@index')->middleware('role:admin')->name('siswa.index');
-=======
 	// Siswa
 	Route::get('/siswa', 'SiswaController@index')->name('siswa.index');
 	Route::post('/siswa', 'SiswaController@importSiswa')->name('siswa.import');
->>>>>>> d8878073db9fc39772b84f30b4c09a18785b6fe5
 });
 
 // Lock screen (sementara)
