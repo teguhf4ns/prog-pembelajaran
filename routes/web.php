3aae3a966738e4ddc889845cbf9df52c444040df
Teguh Firmansyah <teguhfirmansyah@Teguhs-MacBook-Pro.local>
2020-05-05 16:23:15 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+	return view('welcome');
+})->middleware('guest');
 
 Auth::routes();
 
+
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin'], function(){
+Route::group(['prefix' => 'admin'], function () {
+	// Mapel
+	Route::get('/mapel', 'MapelController@index')->name('mapel.index');
+	Route::post('/mapel', 'MapelController@importMapel')->name('mapel.import');
 
+<<<<<<< HEAD
 Route::get('/siswa', 'SiswaController@index')->middleware('role:admin')->name('siswa.index');
+=======
+	// Siswa
+	Route::get('/siswa', 'SiswaController@index')->name('siswa.index');
+	Route::post('/siswa', 'SiswaController@importSiswa')->name('siswa.import');
+>>>>>>> d8878073db9fc39772b84f30b4c09a18785b6fe5
 });
+
+// Lock screen (sementara)
+Route::get('/screenlock', function () {
+	return view('screenlock');
+})->name('screenlock');
