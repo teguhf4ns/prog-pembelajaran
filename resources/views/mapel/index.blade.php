46f2659abf3d363ecf10b8083b3fb4f14647840e
tb12as <syafiqafifuddin59@gmail.com>
2020-05-10 23:54:41 +0700

    Action import mapel

--- resources/views/mapel/index.blade.php
+++ resources/views/mapel/index.blade.php
@@ -48,7 +48,7 @@
               <!-- Import Excel -->
               <div class="modal fade" id="importExcel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
-                  <form method="post" action="/mapel/import_excel" enctype="multipart/form-data">
+                  <form method="post" action="{{ route('mapel.index') }}" enctype="multipart/form-data">
                     <div class="modal-content">
                       <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Import Excel</h5>
